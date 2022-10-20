d933c4e04af91fd559111f567bc378ffb81ddc01
Actanzil <tanziluadjiganesha@gmail.com>
2022-10-21 01:14:29 +0700

    Perubahan Tampilan Output

--- app/src/main/java/com/example/vmld/data/Data.kt
+++ app/src/main/java/com/example/vmld/data/Data.kt
@@ -2,6 +2,6 @@ package com.example.vmld.data
 
 data class Data(val nim: String, val nama: String, val kelas: String){
     override fun toString(): String {
-        return "$nim - $nama - $kelas"
+        return "NIM Mahasiswa   : $nim \nNama Mahaiswa  : $nama \nKelas Mahasiswa   : $kelas"
     }
 }
